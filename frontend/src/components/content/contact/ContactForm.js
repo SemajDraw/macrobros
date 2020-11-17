4a7ba07b73870d55a093880efb5ee3c012f09071
JamesWard01 <james.ward01@liberty-it.co.uk>
2020-11-17 19:49:59 +0000

    Refactored the contact form to trigger validation and use single state object

--- frontend/src/components/content/contact/ContactForm.js
+++ frontend/src/components/content/contact/ContactForm.js
@@ -1,27 +1,30 @@
 import React, {useState} from 'react';
-import {Button, Col, Form} from "react-bootstrap";
+import {Button, Col, Form} from 'react-bootstrap';
 
 export const ContactForm = ({formSubmit}) => {
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [body, setBody] = useState("");
-
+    const [form, setFormState] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        body: ''
+    });
     const [validated, setValidated] = useState(false);
 
+    const handleChange = (e) => {
+        setFormState({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
     const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
+        const target = event.currentTarget;
+        if (target.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
         }
-        formSubmit({
-            'firstName': firstName,
-            'lastName': lastName,
-            'email': email,
-            'body': body,
-        });
+        formSubmit(form);
         setValidated(true);
     };
 
@@ -30,47 +33,60 @@ export const ContactForm = ({formSubmit}) => {
             <Form noValidate validated={validated}
                   onSubmit={handleSubmit}>
                 <Form.Row>
-                    <Form.Group as={Col} controlId="formGridFirstName">
+                    <Form.Group as={Col} controlId='firstName'>
                         <Form.Label>First name</Form.Label>
-                        <Form.Control onChange={(e) => setFirstName(e.target.value)}
-                                      required type="text"
-                                      placeholder="First name"/>
-                        <Form.Control.Feedback type="invalid">
+                        <Form.Control
+                            name='firstName'
+                            onChange={handleChange}
+                            required type='text'
+                            placeholder='First name'/>
+                        <Form.Control.Feedback type='invalid'>
                             Please provide a first name
                         </Form.Control.Feedback>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridLastName">
+                    <Form.Group as={Col} controlId='lastName'>
                         <Form.Label>Last name</Form.Label>
-                        <Form.Control onChange={(e) => setLastName(e.target.value)}
-                                      required type="text"
-                                      placeholder="Last name"/>
-                        <Form.Control.Feedback type="invalid">
+                        <Form.Control
+                            name='lastName'
+                            onChange={handleChange}
+                            required type='text'
+                            placeholder='Last name'/>
+                        <Form.Control.Feedback type='invalid'>
                             Please provide a last name
                         </Form.Control.Feedback>
                     </Form.Group>
                 </Form.Row>
 
-                <Form.Group controlId="formGridEmail">
+                <Form.Group controlId='email'>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control onChange={(e) => setEmail(e.target.value)}
-                                  required type="email"
-                                  placeholder="Enter email"/>
-                    <Form.Control.Feedback type="invalid">
+                    <Form.Control
+                        name='email'
+                        onChange={handleChange}
+                        required
+                        type='email'
+                        placeholder='Enter email'/>
+                    <Form.Control.Feedback type='invalid'>
                         Please provide an email
                     </Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Tell us something...</Form.Label>
-                    <Form.Control onChange={(e) => setBody(e.target.value)}
-                                  required as="textarea" rows="3"/>
-                    <Form.Control.Feedback type="invalid">
+                <Form.Group controlId='body'>
+                    <Form.Label>Ask us anything...</Form.Label>
+
+                    <Form.Control
+                        name='body'
+                        onChange={handleChange}
+                        required
+                        as='textarea'
+                        rows='3'
+                        placeholder='If you have any questions here is the place...'/>
+                    <Form.Control.Feedback type='invalid'>
                         Please tell us something
                     </Form.Control.Feedback>
                 </Form.Group>
 
-                <Button className='btn-block mb-0 mt-4' variant="primary" type="submit">
+                <Button className='btn-block mb-0 mt-4' variant='primary' type='submit'>
                     Submit
                 </Button>
             </Form>
