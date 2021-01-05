d91a9f88e7e8ac24ec078d6555872d6509d0fbd8
JamesWard01 <james.ward01@liberty-it.co.uk>
2021-01-05 19:18:58 +0000

    Added ebextension files to configure the loadbalancer

--- .platform/hooks/prebuild/01login.sh
+++ .platform/hooks/prebuild/01login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DOCKER_USER=/opt/elasticbeanstalk/bin/get-config environment -k DOCKER_USER
 DOCKER_PASSWD=/opt/elasticbeanstalk/bin/get-config environment -k DOCKER_PASSWD
-docker login -u $DOCKER_USER-p $DOCKER_PASSWD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWD
