f36140b5ebc66e7f1bece6248b0c313d52ad46f1
JamesWard01 <james.ward01@liberty-it.co.uk>
2021-01-07 18:43:04 +0000

    Updated the Google Analytics for the site

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,13 +5,19 @@ import AppRouter from './App.Routes';
 import { Provider } from 'react-redux';
 import store from '../store';
 import { loadUser } from '../actions/auth/auth';
+import { createBrowserHistory } from 'history';
 import ReactGa from 'react-ga';
 import './App.scss';
 
+const history = createBrowserHistory();
+
 export const App = () => {
 	useEffect(() => {
-		ReactGa.initialize('G-QY4CZ5ENYG');
-		ReactGa.pageview(window.location.pathname);
+		ReactGa.initialize('UA-186791845-1');
+		history.listen((location) => {
+			ReactGa.set({ page: location.pathname }); // Update the user's current page
+			ReactGa.pageview(location.pathname); // Record a pageview for the given page
+		});
 	}, []);
 
 	store.dispatch(loadUser());
